e4a57db37d395e643052f2fbcce668ec14c066b4
Hafiz Pratama <hafizpratama0809@gmail.com>
2025-11-27 00:54:34 +0700

    Page conntroller

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,5 +1,7 @@
 import 'package:get/get.dart';
 
+import '../modules/PageView/bindings/page_view_binding.dart';
+import '../modules/PageView/views/page_view_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
 import '../modules/login/bindings/login_binding.dart';
@@ -22,6 +24,11 @@ class AppPages {
       name: _Paths.LOGIN,
       page: () => const LoginView(),
       binding: LoginBinding(),
+    ),
+    GetPage(
+      name: _Paths.PAGE_VIEW,
+      page: () => const PageViewView(),
+      binding: PageViewBinding(),
     ),
   ];
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -5,10 +5,12 @@ abstract class Routes {
   Routes._();
   static const HOME = _Paths.HOME;
   static const LOGIN = _Paths.LOGIN;
+  static const PAGE_VIEW = _Paths.PAGE_VIEW;
 }
 
 abstract class _Paths {
   _Paths._();
   static const HOME = '/home';
   static const LOGIN = '/login';
+  static const PAGE_VIEW = '/page-view';
 }
